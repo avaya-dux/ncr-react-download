9706d5873c71ed869b065e88c9938a3a11648667
Chunguang Yang <chungguangy@avaya.com>
2023-04-05 15:37:09 -0400

    fix e2e tests

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
+  video: false,
   e2e: nxE2EPreset(__dirname, {
     bundler: 'vite',
   }),
--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,13 +1,9 @@
-import { getGreeting } from '../support/app.po';
+import { getButtons } from '../support/app.po';
 
 describe('react-download', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome react-download');
+  it('should have 3 download buttons', () => {
+    getButtons().should('have.length', 3);
   });
 });
